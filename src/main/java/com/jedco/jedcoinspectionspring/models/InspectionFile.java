16d5c64eaaeee51f9bed31593a28dbca789ace4d
Mirtineh <mirfil92@gmail.com>
2024-01-04 16:17:00 +0200

    implement some models

---
+++ src/main/java/com/jedco/jedcoinspectionspring/models/InspectionFile.java
@@ -0,0 +1,34 @@
+package com.jedco.jedcoinspectionspring.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "inspection_files")
+public class InspectionFile extends BaseEntity{
+    @Column(name="file_name", nullable=false, length=100)
+    private String fileName;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="file_uploaded_on", length=23)
+    private Date fileUploadedOn;
+
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="inspection_id", nullable=false)
+    private Inspection inspections;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="file_status", nullable=false)
+    private Status status;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="file_uploaded_by", nullable=false)
+    private User user;
+}
