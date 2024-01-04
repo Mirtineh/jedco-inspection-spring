d3f16508acb20680ec9b446082190606e5dcfed6
Mirtineh <mirfil92@gmail.com>
2024-01-04 15:06:37 +0200

    first commit

---
+++ src/main/java/com/jedco/jedcoinspectionspring/JedcoInspectionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jedco.jedcoinspectionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedcoInspectionSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JedcoInspectionSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jedco/jedcoinspectionspring/JedcoInspectionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jedco.jedcoinspectionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JedcoInspectionSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
