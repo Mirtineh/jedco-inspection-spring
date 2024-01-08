412650cd7e141a1cbeef569d5c8308bcb760ffeb
Mirtineh <mirfil92@gmail.com>
2024-01-08 14:07:45 +0200

    implement some repositories

---
+++ src/main/java/com/jedco/jedcoinspectionspring/repositories/SalesAssessmentRepository.java
@@ -0,0 +1,7 @@
+package com.jedco.jedcoinspectionspring.repositories;
+
+import com.jedco.jedcoinspectionspring.models.SalesAssessment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalesAssessmentRepository extends JpaRepository<SalesAssessment,Long> {
+}
