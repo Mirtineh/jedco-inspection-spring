6787d12fc2127652e0813f353eca6ba716abcc11
Mirtineh <mirfil92@gmail.com>
2024-01-11 15:41:10 +0200

    implement inspection service

--- src/main/java/com/jedco/jedcoinspectionspring/repositories/InspectionRepository.java
+++ src/main/java/com/jedco/jedcoinspectionspring/repositories/InspectionRepository.java
@@ -3,5 +3,9 @@ package com.jedco.jedcoinspectionspring.repositories;
 import com.jedco.jedcoinspectionspring.models.Inspection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface InspectionRepository extends JpaRepository<Inspection,Long> {
+    List<Inspection> findAllByStatusId_NotAndRegisteredOnBetween(Long deletedStatus, Date date, Date date1);
 }
