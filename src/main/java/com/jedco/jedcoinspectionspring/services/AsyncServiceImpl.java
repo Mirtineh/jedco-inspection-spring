0d62f4efa13c35b5e06c2ef36d04284a8ab833b8
Mirtineh <mirfil92@gmail.com>
2024-01-11 15:40:57 +0200

    implement inspection service

--- src/main/java/com/jedco/jedcoinspectionspring/services/AsyncServiceImpl.java
+++ src/main/java/com/jedco/jedcoinspectionspring/services/AsyncServiceImpl.java
@@ -1,16 +1,30 @@
 package com.jedco.jedcoinspectionspring.services;
 
 import com.jedco.jedcoinspectionspring.models.TaskHistory;
+import com.jedco.jedcoinspectionspring.repositories.TaskHistoryRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AsyncServiceImpl implements AsyncService {
+    private final TaskHistoryRepository taskHistoryRepository;
     @Override
+    @Async
+    @Transactional(value = Transactional.TxType.REQUIRES_NEW)
     public void postHistory(TaskHistory taskHistory) {
+        log.info(" inserting task history to db ..................... ");
+        try{
 
+            taskHistoryRepository.save(taskHistory);
+        }catch (Exception ex){
+
+               log.error("Task History Insert Details => Failed to register due to "+ex.getMessage());
+
+        }
     }
 }
