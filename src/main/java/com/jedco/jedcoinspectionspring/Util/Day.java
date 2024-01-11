b32b64c9fe75095e0ca7d2f5001998914f12eec6
Mirtineh <mirfil92@gmail.com>
2024-01-11 15:41:02 +0200

    implement inspection service

---
+++ src/main/java/com/jedco/jedcoinspectionspring/Util/Day.java
@@ -0,0 +1,6 @@
+package com.jedco.jedcoinspectionspring.Util;
+
+import java.util.Date;
+
+public record Day(Date startTime, Date endTime) {
+}
--- src/main/java/com/jedco/jedcoinspectionspring/services/CustomerServiceImpl.java
+++ src/main/java/com/jedco/jedcoinspectionspring/services/CustomerServiceImpl.java
@@ -1,16 +1,29 @@
 package com.jedco.jedcoinspectionspring.services;
 
+import com.jedco.jedcoinspectionspring.mappers.CustomerMapper;
+import com.jedco.jedcoinspectionspring.models.Customer;
+import com.jedco.jedcoinspectionspring.repositories.CustomerRepository;
 import com.jedco.jedcoinspectionspring.rest.responses.CustomerResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
     @Override
     public CustomerResponseDto getCustomer(String meterNo) {
-        return null;
+        Optional<Customer> optionalCustomer= customerRepository.findFirstByMeterNo(meterNo);
+        if(optionalCustomer.isEmpty()){
+            return new CustomerResponseDto(false,"Customer Not Found in the system",null);
+        }
+        Customer customer=optionalCustomer.get();
+        var customerResponse= customerMapper.toCustomerResponse(customer);
+        return new CustomerResponseDto(true, "Customer Found in the system",customerResponse);
     }
 }
