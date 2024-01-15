167f7d6269a87e2b7481397eb55876a9768d6072
Mirtineh <mirfil92@gmail.com>
2024-01-15 16:35:04 +0200

    implement InspectionLegalService

--- src/main/java/com/jedco/jedcoinspectionspring/repositories/SalesAssignmentRepository.java
+++ src/main/java/com/jedco/jedcoinspectionspring/repositories/SalesAssignmentRepository.java
@@ -3,5 +3,8 @@ package com.jedco.jedcoinspectionspring.repositories;
 import com.jedco.jedcoinspectionspring.models.SalesAssignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SalesAssignmentRepository extends JpaRepository<SalesAssignment,Long> {
+    Optional<SalesAssignment> findOneByInspectionId(Long inspectionId);
 }
