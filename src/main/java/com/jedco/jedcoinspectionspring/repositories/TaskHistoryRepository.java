12713bb27b4ef0717c56abbd140340d62fa737b2
Mirtineh <mirfil92@gmail.com>
2024-01-16 15:29:59 +0200

    implement task history service

--- src/main/java/com/jedco/jedcoinspectionspring/repositories/TaskHistoryRepository.java
+++ src/main/java/com/jedco/jedcoinspectionspring/repositories/TaskHistoryRepository.java
@@ -3,5 +3,8 @@ package com.jedco.jedcoinspectionspring.repositories;
 import com.jedco.jedcoinspectionspring.models.TaskHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskHistoryRepository extends JpaRepository<TaskHistory,Long> {
+    List<TaskHistory> findAllByInspection_Id(Long taskId);
 }
--- src/main/java/com/jedco/jedcoinspectionspring/services/TaskHistoryServiceImpl.java
+++ src/main/java/com/jedco/jedcoinspectionspring/services/TaskHistoryServiceImpl.java
@@ -1,5 +1,8 @@
 package com.jedco.jedcoinspectionspring.services;
 
+import com.jedco.jedcoinspectionspring.mappers.TaskHistoryMapper;
+import com.jedco.jedcoinspectionspring.models.TaskHistory;
+import com.jedco.jedcoinspectionspring.repositories.TaskHistoryRepository;
 import com.jedco.jedcoinspectionspring.rest.responses.TaskHistoryResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,8 +14,11 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TaskHistoryServiceImpl implements TaskHistoryService {
+    private final TaskHistoryRepository taskHistoryRepository;
+    private final TaskHistoryMapper taskHistoryMapper;
     @Override
     public List<TaskHistoryResponse> getTaskHistory(Long taskId) {
-        return null;
+        List<TaskHistory> taskHistoryList= taskHistoryRepository.findAllByInspection_Id(taskId);
+        return taskHistoryList.stream().map(taskHistoryMapper::toResponse).toList();
     }
 }
