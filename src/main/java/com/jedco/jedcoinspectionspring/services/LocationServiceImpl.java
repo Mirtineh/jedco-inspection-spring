5d913cc18538fae3b7d31ebde19a6ae0c5d21999
Mirtineh <mirfil92@gmail.com>
2024-01-16 10:06:12 +0200

    implement Location Service

--- src/main/java/com/jedco/jedcoinspectionspring/services/LocationServiceImpl.java
+++ src/main/java/com/jedco/jedcoinspectionspring/services/LocationServiceImpl.java
@@ -1,5 +1,8 @@
 package com.jedco.jedcoinspectionspring.services;
 
+import com.jedco.jedcoinspectionspring.mappers.LocationMapper;
+import com.jedco.jedcoinspectionspring.models.Location;
+import com.jedco.jedcoinspectionspring.repositories.LocationRepository;
 import com.jedco.jedcoinspectionspring.rest.responses.LocationResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,8 +14,11 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class LocationServiceImpl implements LocationService {
+    private final LocationRepository locationRepository;
+    private final LocationMapper locationMapper;
     @Override
     public List<LocationResponse> listAll() {
-        return null;
+        List<Location> locations= locationRepository.findAll();
+        return locations.stream().map(locationMapper::toLocationResponse).toList();
     }
 }
