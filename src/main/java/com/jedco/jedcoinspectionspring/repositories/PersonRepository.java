2e34bd4c494e4c6ae6ed783e38ec2dc7d4be8818
Mirtineh <mirfil92@gmail.com>
2024-01-16 10:06:41 +0200

    implement person service

--- src/main/java/com/jedco/jedcoinspectionspring/repositories/PersonRepository.java
+++ src/main/java/com/jedco/jedcoinspectionspring/repositories/PersonRepository.java
@@ -3,5 +3,8 @@ package com.jedco.jedcoinspectionspring.repositories;
 import com.jedco.jedcoinspectionspring.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person,Long> {
+    List<Person> findAllByStatus_Id(Long activeStatus);
 }
