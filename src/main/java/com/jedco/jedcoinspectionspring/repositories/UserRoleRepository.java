c1bde38e68785b801e2268d929a90bf38184c5c1
Mirtineh <mirfil92@gmail.com>
2024-01-16 10:07:50 +0200

    refactor user and user role repository

--- src/main/java/com/jedco/jedcoinspectionspring/repositories/UserRoleRepository.java
+++ src/main/java/com/jedco/jedcoinspectionspring/repositories/UserRoleRepository.java
@@ -3,5 +3,8 @@ package com.jedco.jedcoinspectionspring.repositories;
 import com.jedco.jedcoinspectionspring.models.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRoleRepository extends JpaRepository<UserRole,Long> {
+    Optional<UserRole> findByRoleName(String s);
 }
