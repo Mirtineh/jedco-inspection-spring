fae0ad6559fbaae1ad882f750038ed160e3df8b1
Mirtineh <mirfil92@gmail.com>
2024-01-18 10:17:53 +0200

    add inspection, sales and legal static files

---
+++ src/main/resources/static/inspection_legal/static/js/16.3742462f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[16],{819:function(e,t,c){"use strict";c.r(t);c(0);var i=c(558),s=c(11);t.default=()=>Object(s.jsxs)(i.O,{children:[Object(s.jsx)(i.i,{sm:"6",lg:"3"}),Object(s.jsx)(i.i,{sm:"6",lg:"3"})]})}}]);
+//# sourceMappingURL=16.3742462f.chunk.js.map
