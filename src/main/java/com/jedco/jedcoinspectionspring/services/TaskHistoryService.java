ec3d9fc389842df763c8f73e0b689485bf286efb
Mirtineh <mirfil92@gmail.com>
2024-03-22 09:52:43 +0200

    refactor task history service

--- src/main/java/com/jedco/jedcoinspectionspring/services/TaskHistoryService.java
+++ src/main/java/com/jedco/jedcoinspectionspring/services/TaskHistoryService.java
@@ -1,8 +1,12 @@
 package com.jedco.jedcoinspectionspring.services;
 
+import com.jedco.jedcoinspectionspring.models.Inspection;
+import com.jedco.jedcoinspectionspring.models.Status;
+import com.jedco.jedcoinspectionspring.models.User;
 import com.jedco.jedcoinspectionspring.rest.responses.TaskHistoryResponse;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -10,4 +14,6 @@ public interface TaskHistoryService {
     List<TaskHistoryResponse> getTaskHistory(Long taskId);
 
     ResponseEntity<Resource> downloadFile(Long fileId);
+
+    void insertTaskHistory(Inspection inspection,String note, User user, String actionType, String detailInfo, String fileSeparator, MultipartFile[] files);
 }
